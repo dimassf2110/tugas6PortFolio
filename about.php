821b7e134cad13e65ec2e4ed9010d92443d591c0
dimas2110 <dimasfirdaus927@gmail.com>
2023-04-12 00:03:41 +0700

    tugas6php

---
+++ about.php
@@ -0,0 +1,103 @@
+<!-- ======= About Section ======= -->
+<section id="about" class="about">
+      <div class="container">
+      <?php include_once ('index.php');?>
+        <div class="section-title">
+          <h1>About</h1>
+           </div>
+
+        <div class="row">
+          <div class="col-lg-4" data-aos="fade-right">
+            <img src="assets/img/fotoku.jpeg" class="img-fluid" alt="">
+          </div>
+          <div class="col-lg-8 pt-4 pt-lg-0 content" data-aos="fade-left">
+            <h3>UI/UX Designer &amp; Web Developer.</h3>
+            <p class="fst-italic">
+              HALO SELAMAT DATANG <br>
+              Salam Kenal ya 
+            </p>
+            <div class="row">
+            <div class="about-info">
+                        <p><span class="title-s">Nama: </span> <span>DIMAS SYAHRUL FIRDAUS</span></p>
+                        <p><span class="title-s">Profil: </span> <span>full stack developer</span></p>
+                        <p><span class="title-s">Email: </span> <span>dimasfirdaus927@gmail.com</span></p>
+                        <p><span class="title-s">Telpon: </span> <span>(+62) 822- 6436-7274</span></p>
+                        <p><span class="title-s">Asal Kota: </span> <span>Trenggalek </span></p>  
+                    </div>
+              <div class="col-lg-6">
+                <ul>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Umur:</strong> <span>22</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Status:</strong> <span>Belum Menikah</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Agama:</strong> <span>Islam</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Freelance:</strong> <span>Available</span></li>
+                </ul>
+              </div>
+            </div>
+          
+            <p>
+              Saya merupakan Mahasiswa Jurusan Sistem Informasi Semester 6, Saat ini saya sedang mengikuti program Kampus Merdeka yaitu Studi independen di  PT. Nurul Fikri Cipta (NF Computer) dengan Fokus Pembelajaran 
+                      Fullstack Web Developer.
+            </p>
+            <section id="skills" class="skills section-bg">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>Skills</h2>
+         </div>
+
+        <div class="row skills-content">
+
+          <div class="col-lg-6" data-aos="fade-up">
+
+            <div class="progress">
+              <span class="skill">HTML <i class="val">100%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+            <div class="progress">
+              <span class="skill">CSS <i class="val">90%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+            <div class="progress">
+              <span class="skill">JavaScript <i class="val">75%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+          </div>
+
+          <div class="col-lg-6" data-aos="fade-up" data-aos-delay="100">
+
+            <div class="progress">
+              <span class="skill">PHP <i class="val">80%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+            <div class="progress">
+              <span class="skill">WordPress/CMS <i class="val">90%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+            
+            </div>
+
+          </div>
+
+        </div>
+
+      </div>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End About Section --ss>
---
+++ home.php
@@ -0,0 +1,8 @@
+<!-- ======= Hero Section ======= -->
+<?php include_once ('index.php');?>
+  <section id="hero" class="d-flex flex-column justify-content-center align-items-center">
+    <div class="hero-container" data-aos="fade-in">
+      <h1>DIMAS SYAHRUL FIRDAUS </h1>
+      <p> <span class="typed" data-typed-items=> I'm STUDENT</span></p>
+    </div>
+  </section><!-- End Hero -->
